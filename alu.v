f52bb7bc0323ad16e03db6fba9cac2d9dd37521a
Darren <107428067+darrenang2@users.noreply.github.com>
2025-03-01 20:21:41 +0000

    updated email for all files

--- alu.v
+++ alu.v
@@ -1,11 +1,11 @@
 //=========================================================================
 // Name & Email must be EXACTLY as in Gradescope roster!
-// Name: 
-// Email: 
+// Name: Darren Ang
+// Email: dang004@ucr.edu
 // 
-// Assignment name: 
-// Lab section: 
-// TA: 
+// Assignment name: Lab05-PipelinesAndHazards
+// Lab section: 21
+// TA: Nurlan Nazaraliyev
 // 
 // I hereby certify that I have not received assistance on this assignment,
 // or used code, from ANY outside source other than the instruction team
